9282eb93d6cd0fa08423e1b2afdb07cdd9a01dc0
coding4freedom <chawkins2010@hotmail.com>
2025-01-08 14:51:08 -0600

    containsDup soultion solved and notes added

--- containsDuplicate/index.js
+++ containsDuplicate/index.js
@@ -11,7 +11,54 @@
  */
 
 const containsDuplicate = (nums) => {
+    // Optimal solution 
+    // O(n) time and space
+    const set = new Set()
 
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
+            return true;
+        }
+        set.add(nums[i])
+    }
+    return false;
 }
 
 module.exports = containsDuplicate;
+
+
+/*  solution I deduced
+const containsDuplicate = (nums) => {
+    let reduceSet = [...new Set(nums)];
+
+    return nums.length !== reduceSet.length
+}
+
+*/
+
+/*
+const containsDuplicate = (nums) => {
+    // sorted solution 
+    const sortArr = nums.sort((a, b) => a - b); // O(n) space | O(nlog(n)) time
+
+    for (let i = 0; i < sortArr.length - 1; i++) {
+        if (sortArr[i] === sortArr[i + 1] ) {
+            return true;
+        }
+    }
+    return false;
+}
+*/
+/*
+// Brute force way 
+    // O(n^2) time (explain due to using double for loop) | O(1) space complexity  no addition memeory used to store anything
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            console.log(nums[i], nums[j])
+            if (nums[i] === nums[j]) {                
+                return true
+            }
+        }
+    }
+    return false
+*/
