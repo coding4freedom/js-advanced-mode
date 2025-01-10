757fcfaf104eb0ff20b76d5399c7d7d1c46c68d7
coding4freedom <chawkins2010@hotmail.com>
2025-01-10 17:37:57 -0600

    maxSubarray notes with solution

--- maxSubarray/index.js
+++ maxSubarray/index.js
@@ -10,8 +10,50 @@
   */
 
 const maxSubArray = (nums) => {
+  // O(n) time | O(1) space
+  let maxSum = nums[0];
+  let sum = 0;
 
+  for (let i = 0; i < nums.length; i++) {
+    if ( sum < 0){
+      sum = 0;
+    }
+    sum += nums[i];
+    maxSum = Math.max(maxSum, sum)
+  }
+
+  return maxSum;
 }
 
 //DO NOT EDIT BELOW THIS LINE
 module.exports = maxSubArray;
+
+
+/*
+  // O(n^2) time | O(n) space
+  let maxSum = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;  
+    for (let j = i; j < nums.length; j++) {  
+      sum += nums[j];
+      maxSum = Math.max(maxSum, sum)
+    }
+  }
+  return maxSum;
+
+-------------------------------------------------
+  O(n^3) time | O(n) space
+  let maxSum = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {  // O(n) T becomes O(n) * O(n^2) = O(n^3)
+    for (let j = i + 1; j <= nums.length; j++) {  // O(n) T becomes O(n) * O(n) = O(n^2)
+      const subArr = nums.slice(i, j);  // O(n) T
+      const sum = subArr.reduce((acc, curr) => acc + curr);  // O(n) T
+      maxSum = Math.max(maxSum, sum)
+    }
+  }
+
+  return maxSum;
+
+*/
