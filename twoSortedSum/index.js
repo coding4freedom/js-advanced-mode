60b295b3e631725958b8166c6ec8826c74a38a5f
coding4freedom <chawkins2010@hotmail.com>
2025-01-12 20:23:04 -0600

    twoSorted sum notes added with solution

--- twoSortedSum/index.js
+++ twoSortedSum/index.js
@@ -10,9 +10,22 @@
  * twoSum([2, 3, 4], 6) -> [0, 2]
  * 
  */
-
+// O(n) time | O(1) space
 const twoSortedSum = (nums, target) => {
+   let l = 0;
+   let r = nums.length - 1;
 
+   while (l < r) {
+        const sum = nums[l] + nums[r];
+
+        if (sum > target) {
+            r--;
+        } else if (sum < target) {
+            l++;
+        } else {
+            return [l, r]
+        }
+   }
 }
 
 module.exports = twoSortedSum;
