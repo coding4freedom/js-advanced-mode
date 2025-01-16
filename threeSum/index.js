b9de3bb58b79ba3b539514b6ea1c843f3bd9d5df
coding4freedom <chawkins2010@hotmail.com>
2025-01-16 17:13:21 -0600

    threeSum solution added

--- threeSum/index.js
+++ threeSum/index.js
@@ -12,7 +12,35 @@
  */
 
 function threeSum(nums) {
-  
+  nums.sort((a, b) => a - b);
+  const result = [];
+
+  for (i = 0; i < nums.length - 2; i++) {
+    if (nums[i] === nums[i - 1]) {
+        continue;
+    }
+    let l = i + 1;
+    let r = nums.length - 1;
+
+    while (l < r) {
+        const sum = nums[i] + nums[l] + nums[r];
+
+        if (sum > 0) {
+            r--;
+        }
+        if (sum < 0) {
+            l++;
+        }
+        if (sum == 0) {
+            result.push([nums[i], nums[l], nums[r]]);
+            l++;
+            while (nums[l] === nums[l - 1] && l < r) {
+                l++;
+            }
+        }
+    }
+  }
+  return result;
 }
 
 //DO NOT EDIT BELOW THIS LINE
